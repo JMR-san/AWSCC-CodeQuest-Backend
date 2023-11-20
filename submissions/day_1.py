d393073e9ac3dc44338586c0e27a9b89e57b522b
Joem <83769810+JMR-san@users.noreply.github.com>
2023-11-20 22:09:54 +0800

    Create var.py

---
+++ submissions/day_1.py
@@ -0,0 +1,5 @@
+your_name = "Joemar"
+name_of_department = "UI/UX"
+about_you = "I love exploring things"
+
+print(f"Hello, I am {your_name}. I love {name_of_department} and I love being in the Backend even more! One thing about me is that {about_you}.")
