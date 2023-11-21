ddf41d14286cb21e943901198153978452918242
Joem <83769810+JMR-san@users.noreply.github.com>
2023-11-21 21:59:07 +0800

    Update day_2.py

---
+++ submissions/day_2.py
@@ -0,0 +1,12 @@
+
+first = input("Enter your first number: ")
+print (f"Your first number is {first}.")
+
+second = input("Enter your second number: ")
+print (f"Your second number is {second}.")
+
+third = input("Enter your third number: ")
+print (f"Your third number is {third}")
+
+sum = int(first) + int(second) + int(third)
+print (f"The sum of all numbers is {sum}.")
