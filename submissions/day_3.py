da7d52fd577428f92a877cce898944705c2e4e21
Joem <83769810+JMR-san@users.noreply.github.com>
2023-11-21 23:15:23 +0800

    This is the challenge time

--- submissions/day_3.py
+++ submissions/day_3.py
@@ -1,10 +1,23 @@
+playerName = input("What is your name? ")
+print (f"Hello, {playerName}!")
 
-first = input("Enter a number: ")
+answer = input("Can you help me find a shelter? (Y / N)")
 
-if int(first) < 0:
-    print (f"{first} is a Negative number!")
-elif int(first) > 0:
-    print (f"{first} is a Postive number!")
-else:
-    print ("That is a Zero!")
+if answer.capitalize() == 'Y':
+    print ("The Police arived")
+    pol = input ("Is the thief inside? (Y / N) ")
+
+    if pol.capitalize() == "Y":
+        print (f"The thief was caught, {playerName} wins!")
+    elif pol.capitalize() != "Y":
+        print (f"The thief escaped! {playerName} lose")
+
+if answer.capitalize() != 'Y':
+    print (f"The thief attacked {playerName}")
+    atk = input ("Are you going to fightback? (Y / N)")
+
+    if atk.capitalize() == "Y":
+        print (f"The thief was defeated, {playerName} wins!")
+    else:
+        print (f"The thief escaped! {playerName} lose")
 
---
+++ submissions/day_3_mini.py
@@ -0,0 +1,10 @@
+
+first = input("Enter a number: ")
+
+if int(first) < 0:
+    print (f"{first} is a Negative number!")
+elif int(first) > 0:
+    print (f"{first} is a Postive number!")
+else:
+    print ("That is a Zero!")
+
